3eda3e2929a81ad3aa367c0b4f737769c15ce32a
Lao·Zhu <2208213223@qq.com>
2021-08-25 09:39:29 +0800

    add windows_check_install image

--- program/build_fw.bat
+++ program/build_fw.bat
@@ -2,3 +2,5 @@ md cmake-build-debug
 cd cmake-build-debug
 cmake.exe -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DEPENDS_USE_COMPILER=FALSE -G "CodeBlocks - MinGW Makefiles" ..\
 cmake.exe --build .\ --target miniFOC.elf -- -j 12
+echo Build Firmware Success!!!
+pause 0
