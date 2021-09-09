2123f37e4860b6feccdd70a8a4dc2459094a1bdf
Lao·Zhu <2208213223@qq.com>
2021-09-10 00:31:32 +0800

    The function of opening and closing serial port is realized in the upper computer.

--- tools/mainwindow.cpp
+++ tools/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    refresh_serial_port();
+    ui->serial_baudrate_txt->setText("115200");
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,43 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+bool MainWindow::set_serial_badurate(){
+    if(ui->serial_port_cb->currentText() == NULL || ui->serial_baudrate_txt->toPlainText() == NULL)
+    {
+        QMessageBox::information(NULL, "Error", "Please select port number and baudrate first!");
+        return false;
+    }
+    else
+    {
+        serial.setPortName(ui->serial_port_cb->currentText());
+        serial.setBaudRate(ui->serial_baudrate_txt->toPlainText().toInt());
+        return true;
+    }
+}
+
+void MainWindow::refresh_serial_port()
+{
+    QList<QSerialPortInfo> serialPortinfo = QSerialPortInfo::availablePorts();
+    int count = serialPortinfo.count();
+    ui->serial_port_cb->clear();
+    for(int i = 0; i < count; i++)
+        ui->serial_port_cb->addItem(serialPortinfo.at(i).portName());
+}
+
+void MainWindow::on_open_btn_clicked()
+{
+    if(serial.isOpen() == false){
+        if(set_serial_badurate() == true)
+            serial.open(QSerialPort::ReadWrite);
+        ui->open_btn->setText("Close");
+    }
+    else{
+        serial.close();
+        ui->open_btn->setText("Open");
+    }
+}
+
+void MainWindow::on_refresh_btn_clicked()
+{
+    refresh_serial_port();
+}
--- tools/mainwindow.h
+++ tools/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_refresh_btn_clicked();
+
+    void on_open_btn_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSerialPort serial;
+    void refresh_serial_port();
+    bool set_serial_badurate();
 };
 #endif // MAINWINDOW_H
